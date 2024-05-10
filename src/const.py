b89be69028c08b58c12ccc6a52176b34a92b4ac7
semapheur <danfyll@hotmail.com>
2024-05-10 23:05:28 +0200

    Deploy

---
+++ src/const.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+WORK_DIR = Path(__file__).resolve().parent.parent
+DB_DIR = WORK_DIR / 'data'
+STATIC_DIR = WORK_DIR / 'assets'
+
+HEADERS = {
+  'User-Agent': (
+    'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/112.0'
+  ),
+  'Accept': 'application/json, text/plain, */*',
+  'Accept-Language': 'en-US,en;q=0.5',
+  'Connection': 'keep-alive',
+  #'Accept-Encoding': 'gzip, deflate, br',
+  #'DNT': '1',
+  #'Sec-Fetch-Dest': 'empty',
+  #'Sec-Fetch-Mode': 'cors',
+  #'Sec-Fetch-Site': 'same-site',
+  #'Sec-GPC': '1',
+  #'Cache-Control': 'no-cache',
+}
